09cea5b45d42029d5e270fe34d4a495e124aad04
Ranjith Zachariah <ranjith.zachariah@gmail.com>
2022-04-19 10:38:30 -0400

    Setup
    https://serverless-stack.com/examples/how-to-create-an-apollo-graphql-api-with-serverless.html

---
+++ src/lambda.ts
@@ -0,0 +1,24 @@
+import { gql, ApolloServer } from "apollo-server-lambda";
+
+const IS_LOCAL = !!process.env.IS_LOCAL;
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => "Hello, World!",
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  playground: false,
+  introspection: IS_LOCAL,
+});
+
+export const handler = server.createHandler();
